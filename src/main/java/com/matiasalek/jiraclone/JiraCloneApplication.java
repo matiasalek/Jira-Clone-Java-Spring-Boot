a4e28f291fba6fc65d43dab335015a430dda46b8
Matias Aleksandrowicz <matias.aleksandrowicz@gmail.com>
2025-07-14 22:54:00 -0300

    Initial commit

---
+++ src/main/java/com/matiasalek/jiraclone/JiraCloneApplication.java
@@ -0,0 +1,11 @@
+package com.matiasalek.jiraclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraCloneApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JiraCloneApplication.class, args);
+	}
+}
---
+++ src/test/java/com/matiasalek/jiraclone/JiraCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matiasalek.jiraclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
