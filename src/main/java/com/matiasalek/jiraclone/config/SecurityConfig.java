dc1428f067f17096f1096fa6b9747fc4dc3d49a6
Matias Aleksandrowicz <matias.aleksandrowicz@gmail.com>
2025-08-04 14:55:23 -0300

    Updated SecurityConfig for testing purpouses. Added .bru files to the project, currently testing user endpoints

--- src/main/java/com/matiasalek/jiraclone/config/SecurityConfig.java
+++ src/main/java/com/matiasalek/jiraclone/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.matiasalek.jiraclone.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,8 +18,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/users/**").permitAll()
+                        .requestMatchers("/**").permitAll()
                         .anyRequest().authenticated()
                 );
 
