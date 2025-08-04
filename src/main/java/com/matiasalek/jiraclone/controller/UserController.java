27f664afb62860f165a5d4892fa20d4da1e07e55
Matias Aleksandrowicz <matias.aleksandrowicz@gmail.com>
2025-08-04 14:14:52 -0300

    Added User controller with basic endpoints

---
+++ src/main/java/com/matiasalek/jiraclone/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.matiasalek.jiraclone.controller;
+
+import com.matiasalek.jiraclone.dto.request.CreateUserRequest;
+import com.matiasalek.jiraclone.dto.response.CreateUserResponse;
+import com.matiasalek.jiraclone.entity.User;
+import com.matiasalek.jiraclone.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<CreateUserResponse> createUser(@RequestBody CreateUserRequest user) {
+        CreateUserResponse createdUser = userService.createUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUserById(@PathVariable Long id) {
+        userService.deleteUserById(id);
+        return ResponseEntity.ok().build();
+    }
+}
