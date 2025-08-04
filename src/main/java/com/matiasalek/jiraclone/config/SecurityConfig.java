c4cfcb03030c64496a5a340e0c9ce87da30a52d2
Matias Aleksandrowicz <matias.aleksandrowicz@gmail.com>
2025-08-04 11:39:46 -0300

    Re enable security + corrected typo in security config

--- src/main/java/com/matiasalek/jiraclone/config/SecurityConfig.java
+++ src/main/java/com/matiasalek/jiraclone/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authz -> authz
+                .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/users/**").permitAll()
                         .anyRequest().authenticated()
                 );
