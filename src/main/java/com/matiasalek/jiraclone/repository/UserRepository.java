63efda9d93d5a92844e61ce97dc8281b0b90114e
Matias Aleksandrowicz <matias.aleksandrowicz@gmail.com>
2025-08-05 09:55:57 -0300

    Removed unused orm

--- src/main/java/com/matiasalek/jiraclone/repository/UserRepository.java
+++ src/main/java/com/matiasalek/jiraclone/repository/UserRepository.java
@@ -3,10 +3,7 @@ package com.matiasalek.jiraclone.repository;
 import com.matiasalek.jiraclone.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
-    Optional<User> findByUsername(String username);
 }
